25cb2d3f2970e4ba53220fcd1424ccfdfb0dbff8
mymobil_dev <mymobildev@gmail.com>
2024-03-03 00:55:37 +0000

    adds google authentication

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:geocoding/geocoding.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(const MyApp());
 
